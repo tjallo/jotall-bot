26e6ae66165ee2986b5630d9b91ffe11e62897ab
Tjalle <noreply@tjalle.nl>
2025-12-03 21:36:17 +0100

    feat: add dice command with handling logic and integrate into command registration

--- src/commands/handler.ts
+++ src/commands/handler.ts
@@ -1,17 +1,18 @@
 import type {
-  APIApplicationCommandInteractionData,
   APIGuildMember,
   APIUser,
+  APIUserApplicationCommandInteractionData,
 } from "discord-api-types/v10";
 import { Commands } from "../consts/commands.ts";
 import { handlePing } from "./util.ts";
 import { USER_SERVICE } from "../db/init.ts";
+import { handleDiceCommand } from "./games.ts";
 
 export function handleApplicationCommands(
-  data: APIApplicationCommandInteractionData,
+  data: APIUserApplicationCommandInteractionData,
   user?: APIUser,
   member?: APIGuildMember,
-) {
+): { status: number; body: Record<string, unknown> } {
   const { name } = data;
 
   const commandUser = member?.user ?? user;
@@ -22,7 +23,10 @@ export function handleApplicationCommands(
 
   switch (name) {
     case Commands.Ping:
-      return { status: 200, body: handlePing() };
+      return handlePing();
+
+    case Commands.Dice:
+      return handleDiceCommand(data);
 
     default:
       return { status: 400, body: { error: "unknown command" } };
