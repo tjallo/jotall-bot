dc85488f54844e9a3d602bd818fab763844ec258
Tjalle <noreply@tjalle.nl>
2025-12-11 20:54:01 +0100

    feat: add Minecraft server configuration to environment and update config handling

--- src/consts/config.ts
+++ src/consts/config.ts
@@ -12,6 +12,10 @@ export const APP_ID = getEnvVar("APP_ID");
 export const DISCORD_TOKEN = getEnvVar("DISCORD_TOKEN");
 export const PUBLIC_KEY = getEnvVar("PUBLIC_KEY");
 
+export const MINECRAFT_SERVER_SECRET = getEnvVar("MINECRAFT_SERVER_SECRET");
+export const MINECRAFT_SERVER_IP = getEnvVar("MINECRAFT_SERVER_IP");
+export const MINECRAFT_SERVER_PORT = getEnvVar("MINECRAFT_SERVER_PORT");
+
 export const TMP_DIR = path.join(Deno.cwd(), "tmp");
 export const COMMAND_HASH_FILE = path.join(TMP_DIR, "command_hash.txt");
 
